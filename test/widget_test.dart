2616fd89cb1a37e9578c83dc4e0db61de4f05c60
musa jahun <msjahun@live.com>
2022-08-29 15:52:32 +0100

    fixed tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,15 +16,14 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Splash screen Test'), findsOneWidget);
+
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+ /*   await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();*/
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Splash screen Test'), findsNothing);
   });
 }

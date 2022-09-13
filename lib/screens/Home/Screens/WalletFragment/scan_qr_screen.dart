73d5dddbb9d9254371053bb5206d0639e9ac9f6e
musa jahun <msjahun@live.com>
2022-09-13 10:57:34 +0100

    added overlay to the scan to pay qr code scanner

--- lib/screens/Home/Screens/WalletFragment/scan_qr_screen.dart
+++ lib/screens/Home/Screens/WalletFragment/scan_qr_screen.dart
@@ -17,6 +17,7 @@ class StateScanQrScreen extends State<ScanQrScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          elevation: 0,
           centerTitle: true,
           backgroundColor: Colors.black,
           title: const Text(
@@ -24,108 +25,134 @@ class StateScanQrScreen extends State<ScanQrScreen> {
             style: TextStyle(fontSize: 18),
           ),
         ),
-        body: MobileScanner(
-            allowDuplicates: false,
-            controller: cameraController,
-            onDetect: (barcode, args) {
-              if (barcode.rawValue == null) {
-                debugPrint('Failed to scan Barcode');
-              } else {
-                final String code = barcode.rawValue!;
-                showGeneralDialog(
-                    context: context,
-                    barrierDismissible: true,
-                    barrierLabel: 'barrier text',
-                    transitionDuration: const Duration(milliseconds: 400),
-                    pageBuilder: (bc, ania, anis) {
-                      return Dialog(
-                        child: Container(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const Padding(padding: EdgeInsets.all(8)),
-                              const Text(
-                                'Are you sure you want to pay',
-                                style: TextStyle(
-                                    fontSize: 15, color: Styles.colorPrimary),
-                              ),
-                              Text(
-                                code,
-                                style: const TextStyle(
-                                    fontSize: 15, color: Styles.colorPrimary),
-                              ),
-                              const Text(
-                                'a sum of',
-                                style: TextStyle(
-                                    fontSize: 15, color: Styles.colorPrimary),
-                              ),
-                              const Padding(padding: EdgeInsets.all(8)),
-                              const Text(
-                                'N2,300',
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              const Padding(padding: EdgeInsets.all(16)),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                crossAxisAlignment: CrossAxisAlignment.center,
+        body: Stack(
+          children: [
+            MobileScanner(
+                allowDuplicates: false,
+                controller: cameraController,
+                onDetect: (barcode, args) {
+                  if (barcode.rawValue == null) {
+                    debugPrint('Failed to scan Barcode');
+                  } else {
+                    final String code = barcode.rawValue!;
+                    showGeneralDialog(
+                        context: context,
+                        barrierDismissible: true,
+                        barrierLabel: 'barrier text',
+                        transitionDuration: const Duration(milliseconds: 400),
+                        pageBuilder: (bc, ania, anis) {
+                          return Dialog(
+                            child: Container(
+                              padding: const EdgeInsets.all(16),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
                                 children: [
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                      backgroundColor: Styles.colorBackground,
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 0, horizontal: 30),
-                                      shape: const StadiumBorder(),
-                                    ),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text(
-                                      'Cancel',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.normal,
-                                          color: Styles.colorBlack),
-                                    ),
+                                  const Padding(padding: EdgeInsets.all(8)),
+                                  const Text(
+                                    'Are you sure you want to pay',
+                                    style: TextStyle(
+                                        fontSize: 15, color: Styles.colorPrimary),
                                   ),
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                      backgroundColor: Styles.colorSecondary,
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 0, horizontal: 30),
-                                      shape: const StadiumBorder(),
-                                    ),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Row(
-                                      children: const [
-                                        Icon(
-                                          Gomart.qrcodeScan,
-                                          size: 20,
-                                          color: Styles.colorPrimary,
+                                  Text(
+                                    code,
+                                    style: const TextStyle(
+                                        fontSize: 15, color: Styles.colorPrimary),
+                                  ),
+                                  const Text(
+                                    'a sum of',
+                                    style: TextStyle(
+                                        fontSize: 15, color: Styles.colorPrimary),
+                                  ),
+                                  const Padding(padding: EdgeInsets.all(8)),
+                                  const Text(
+                                    'N2,300',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  const Padding(padding: EdgeInsets.all(16)),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      TextButton(
+                                        style: TextButton.styleFrom(
+                                          backgroundColor: Styles.colorBackground,
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 0, horizontal: 30),
+                                          shape: const StadiumBorder(),
                                         ),
-                                        Padding(padding: EdgeInsets.all(4)),
-                                        Text(
-                                          'Pay',
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text(
+                                          'Cancel',
                                           style: TextStyle(
-                                              fontSize: 15,
                                               fontWeight: FontWeight.normal,
-                                              color: Styles.colorPrimary),
+                                              color: Styles.colorBlack),
                                         ),
-                                      ],
-                                    ),
+                                      ),
+                                      TextButton(
+                                        style: TextButton.styleFrom(
+                                          backgroundColor: Styles.colorSecondary,
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 0, horizontal: 30),
+                                          shape: const StadiumBorder(),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Row(
+                                          children: const [
+                                            Icon(
+                                              Gomart.qrcodeScan,
+                                              size: 20,
+                                              color: Styles.colorPrimary,
+                                            ),
+                                            Padding(padding: EdgeInsets.all(4)),
+                                            Text(
+                                              'Pay',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.normal,
+                                                  color: Styles.colorPrimary),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
                                   ),
+                                  const Padding(padding: EdgeInsets.all(8)),
                                 ],
                               ),
-                              const Padding(padding: EdgeInsets.all(8)),
-                            ],
-                          ),
-                        ),
-                      );
-                    });
-              }
-            }));
+                            ),
+                          );
+                        });
+                  }
+                }),
+            _getOverlay(),
+          ],
+        ));
   }
+}
+Widget _getOverlay() {
+  return ClipPath(
+    clipper: InvertedClipper(),
+    child: Container(
+      color: Colors.black54,
+    ),
+  );
+}
+
+class InvertedClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    return Path()
+      ..addRect(Rect.fromLTWH(0, 0, size.width, size.height))
+      ..addRect(Rect.fromCenter(center: Offset(size.width *0.5, size.height*0.5),height:size.width *0.7, width: size.width *0.7))
+      ..fillType = PathFillType.evenOdd;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }

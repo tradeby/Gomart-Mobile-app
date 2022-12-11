555347afd80001a71946e401bfa03cd3040d48bb
musa jahun <msjahun@live.com>
2022-12-11 15:17:38 +0100

    updated firebase options for our gomart app

--- lib/service/firebase_service.dart
+++ lib/service/firebase_service.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 
 import '../firebase_options.dart';
 
+
+
 class FirebaseService{
   static Future<FirebaseService> init() async {
     await Firebase.initializeApp(

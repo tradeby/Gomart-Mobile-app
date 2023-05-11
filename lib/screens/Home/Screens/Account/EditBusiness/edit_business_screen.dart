e8246b8ebcaad0b8686b6cf26980bc2a6c639eb8
musa jahun <msjahun@live.com>
2023-05-11 05:05:10 +0100

    fixed google prediction issue and also fixed add store gallary photos issues, turns out It was only working in debug mode

--- lib/screens/Home/Screens/Account/EditBusiness/edit_business_screen.dart
+++ lib/screens/Home/Screens/Account/EditBusiness/edit_business_screen.dart
@@ -4,9 +4,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
-
 import '../../../../../styles/custom_home_icons.dart';
 import '../../../../../styles/styles.dart';
 import '../business_profile_screen.dart';
@@ -329,21 +329,7 @@ class EditBusinessScreen extends StatelessWidget {
                   style: TextStyle(fontSize: 14, color: Styles.colorPrimary),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(8),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      'Enter address',
-                      style: TextStyle(color: Styles.colorTextBlack),
-                    ),
-                    Padding(padding: EdgeInsets.all(2)),
-                    BTextField()
-                  ],
-                ),
-              ),
+              EnterAddressAutoComplete(),
               const StoreGallerySection(),
               const Padding(padding: EdgeInsets.all(6)),
               Center(
@@ -380,6 +366,96 @@ class EditBusinessScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class EnterAddressAutoComplete extends StatefulWidget {
+  const EnterAddressAutoComplete({
+    super.key,
+  });
+
+  @override
+  State<StatefulWidget> createState() => EnterAddressAutoCompleteState();
+}
+
+class EnterAddressAutoCompleteState extends State<EnterAddressAutoComplete> {
+  final _textController = TextEditingController();
+  final _places =
+      GoogleMapsPlaces(apiKey: 'AIzaSyDo0KVqGLzCqIUks4a8UJSuAJSW_k3ec3o');
+  List<Prediction> _suggestions = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Enter address',
+            style: TextStyle(color: Styles.colorTextBlack),
+          ),
+          const Padding(padding: EdgeInsets.all(2)),
+          SizedBox(
+            height: 32,
+            child: TextField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                  focusColor: Styles.colorGray,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Styles.colorTextFieldBorder,
+                        width: 0.8,
+                        style: BorderStyle.solid,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5)))),
+              onChanged: _onTextChanged,
+            ),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: _suggestions.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(_suggestions[index].description as String),
+                onTap: () => _onSuggestionSelected(_suggestions[index]),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onTextChanged(String value) async {
+    final response = await _places.autocomplete(
+      value,
+      components: [Component(Component.country, 'ng')],
+    );
+
+    print(response.predictions);
+
+    setState(() {
+      _suggestions = response.predictions;
+    });
+  }
+
+  void _onSuggestionSelected(Prediction prediction) async {
+    final details =
+        await _places.getDetailsByPlaceId(prediction.placeId as String);
+
+    double? lat =  details.result.geometry?.location.lat;
+    double? lng =  details.result.geometry?.location.lng;
+
+    print('$lat, $lng');
+    // do something with the place details
+    setState(() {
+      _suggestions = [];
+      _textController.text = prediction.description as String;
+    });
   }
 }
 
